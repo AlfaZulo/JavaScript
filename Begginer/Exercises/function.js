802ed007ece8f3ac190192a9b0f435e4bd018870
Antonio Zlatkov <100520728+AlfaZulo@users.noreply.github.com>
2022-07-09 22:00:57 +0300

    update

---
+++ Begginer/Exercises/function.js
@@ -0,0 +1,8 @@
+// Function `findSquare()` returns the square of the input number
+//  which is passed as an argument in the function call.
+
+function findSquare(num){
+    return num*num
+}
+
+console.log(findSquare(2));
